4394e1b5036c414a514bd25ca0255f61250845cd
ArildoJunior <arildoajunior@gmail.com>
2021-06-07 08:49:06 -0300

    Validation

--- SalesWeb/Models/Seller.cs
+++ SalesWeb/Models/Seller.cs
@@ -8,16 +8,24 @@ namespace SalesWeb.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "Name required")]
+        [StringLength(60, MinimumLength =3, ErrorMessage = "Name size should be between 3 and 60")]
         public string Name { get; set; }
 
         [DataType (DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [Required(ErrorMessage = "Email required")]
         public string Email { get; set; }
 
         [Display (Name = "Birth Date")]
         [DataType (DataType.Date)]
         [DisplayFormat (DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Birth Date required")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Base Salary")]
+        [Range(100.0, 50000.0, ErrorMessage = "Base Salary must be from 100.0 to 50000.0")]
         [Display (Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
